d93da18b57b8a431036dd40ac96b5ddd9605485d
a15995 <123@123.dk>
2016-11-15 13:12:50 +0100

    15-11-2016

---
+++ ConsoleApplication1/Extensions/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class MyExtensions
+    {
+        public static int Times1000(this int myThousands) // Method extension - defineres med "this"
+        {
+            int x = (myThousands * 1000);
+            return x;             
+        }
+    }
+}
---
+++ ConsoleApplication1/Extensions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 23;
+            Debug.WriteLine(x.Times1000()); // I stedet for alm. metode (kaldes i parantes) kan man kalde objektet (extension)
+        }
+    }
+}
